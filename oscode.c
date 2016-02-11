ca28fcbcbcd7edbd160683de2b5daaf5809adb15
Felix Dube <felix.dube@mail.mcgill.ca>
2016-02-11 15:11:20 -0500

    Add header file

--- oscode.c
+++ oscode.c
@@ -1,12 +1,15 @@
-/***********************************************************************/
-// File: oscode.c
-// Description: Implementation of a simple shell
-// Features:    History of the last 10 commands,
-//              background processes,
-//              output redirection
-// Date: February 2016
-// Author: Felix Dube 260533620
-/***********************************************************************/
+/**
+    ******************************************************************************
+    * File              : oscode.c
+    * Description       : Implementation of a simple shell
+    * Features          : History of the last 10 commands,
+                            background processes, list of jobs in background
+                            output redirection
+    * Author            : Felix Dube 260533620
+    * Version           : 1.0.0
+    * Date              : February 2016
+    ******************************************************************************
+    */
 
 #include <stdio.h>
 #include <unistd.h>
@@ -15,294 +18,8 @@
 #include <sys/mman.h>
 #include <limits.h>
 #include <math.h>
+#include "oscode.h"
 
-// TODO:    Delete head job
-//          background in history
-//          args malloc
-
-/*********************/
-/******** JOB ********/        
-/*********************/
-
-// store in a linked list all the jobs in background, along with their PID
-struct job
-{
-    char *args[20];
-    int pid;
-    struct job * next;
-};
-
-/* removejob */
-// DESCRIPTION  remove a job with a particuliar PID
-// INPUT        struct ** head          pointer to a pointer to the head of linked list of jobs
-//              int pid                 pid of the job to be removed from the linked list
-// RETURN       void
-void removejob(struct job ** head, int pid){
-    struct job * current = *head;
-    struct job * temp_job = NULL;
-
-    // if the job to be removed is the head
-    if (current->pid == pid){
-        temp_job = *head;
-        *head = (*head)->next;
-        free(temp_job);
-        return;
-    }
-
-    // if not we need to find where is the job
-    while(current->next != NULL && current->next->pid != pid){
-        current = current->next;
-    }
-    if(current->next != NULL){
-        temp_job = current->next;
-        current->next = temp_job->next;
-        free(temp_job);
-    }
-
-}
-
-/* printjobs */
-// DESCRIPTION  print all the jobs in back ground and remove the job that are done
-// INPUT        struct job * head       linked list of job to be printed and updated
-// RETURN       void
-void printjobs(struct job ** head) {
-    int i = 0;
-    int status;
-    struct job * current = *head;
-    if (current == NULL) {
-        printf("No job in background.\n");
-    }
-    else{
-        printf("Jobs PID\t\tJobs\n----------------------------------------------------------\n");
-    }
-    while (current != NULL) {
-        printf("%i\t\t", current->pid);
-
-        int j = 0;
-        while(current->args[j] !=  NULL){
-            printf("%s\t", current->args[j]);
-            j++;
-        }
-        printf("\n");
-
-        int pid = current->pid;
-        current = current->next;
-        waitpid(pid, &status, WNOHANG);
-        if(waitpid(pid, &status, WNOHANG) == -1){
-            removejob(head, pid);
-        }
-    }
-}
-
-/* pushjob */
-// DESCRIPTION  add a job to the linked list
-// INPUT        struct job ** head      pointer to a pointer to the head of linked list of jobs
-//              int PID                 pid of the job to be added
-//              char *args[]            command that the job is doing
-// RETURN       void
-void pushjob(struct job ** head, int pid, char *args[]) {
-    struct job * new_job;
-    new_job = malloc(sizeof(struct job));
-
-    new_job->pid = pid;
-    new_job->next = *head;
-    int j = 0 ;
-    for (j = 0; j < 20; j++) {
-        new_job->args[j] = args[j];
-    }
-    *head = new_job;
-}
-
-
-/*************************/
-/******** HISTORY ********/        
-/*************************/
-
-// store the command allong with its history ID
-struct cmd
-{
-    char *args[20];
-    int nbr;
-};
-
-
-/*  addhistory  */
-// DESCRIPTION  add a specific cmd to the history    
-// INPUT        struct cmd *history[]   array in which the command need to be added
-//              char *args[]            command to be added
-//              int nbr                 history ID
-// RETURN       void
-void addhistory(struct cmd *history[], char *args[], int nbr){
-
-    // delete the first element of history and add the new command at position 9
-    if (nbr > 10){
-        int j;
-        for (j = 0; j< 9; j++){
-            history[j] = history[j+1];
-        }
-        history[9] = (struct cmd*) malloc(sizeof(struct cmd));
-        for (j = 0; j < 20; j++) {
-            history[9]->args[j] = args[j];
-        }
-        history[9]->nbr = nbr;
-    }
-
-    // add the new command at the first empty spot in the history
-    else{
-        int j;
-        for (j = 0; j < 20; j++) {
-            history[nbr -1]->args[j] = args[j];
-        }
-        history[nbr -1]->nbr = nbr;
-    }
-    return;
-}
-
-/*  searchhistory   */
-// DESCRIPTION  search for a specific history ID in the history
-// INPUT        struct cmd *history[]   history array to be search in
-//              int nbr                 history ID being search for
-// RETURN       if history ID found it returns the index of the cmd in the history array
-//              otherwise return -1
-int searchhistory(struct cmd *history[], int nbr) {
-    int i;
-    for (i = 0; i < 10; ++i)
-    {
-        if (history[i]->nbr == nbr){
-            return i;
-        }
-    }
-
-    return -1;
-}
-
-/*  isnumber  */
-// DESCRIPTION  verify if a string is a number
-// INPUT        char *string    pointer to the string to be verified
-// RETURN       1 if the string is a number
-//              0 otherwise
-int isnumber (char *string) {
-    int num;
-    num = atoi( string );
- 
-    if (num == 0 && string[0] != '0')
-       return 0;    //false
-    else
-       return 1;    //true
-}
-
-/*  printhistory  */
-// DESCRIPTION  print all commands in the history
-// INPUT        struct cmd *history[]   pointer to the history array
-// RETURN       void
-void printhistory(struct cmd *history[]) {
-    int i = 0;
-    if (history[0]->nbr == 0) {
-        printf("Nothing in history.\n");
-    }
-    else{
-        printf("History ID\t\tCommand\n----------------------------------------------------------\n");
-    }
-    while (i < 10 && history[i]->nbr != 0) {
-        printf("%i\t\t\t", history[i]->nbr);
-        int j = 0;
-        while(history[i]->args[j] !=  NULL){
-            printf("%s\t", history[i]->args[j]);
-            j++;
-        }
-        printf("\n");
-        i++;
-    }
-}
-
-/* stringtoint */
-// DESCRIPTION  cast a string to int
-// INPUT        char* a     pointer to the string to be cast to int
-// RETURN       index       number that was in the string
-int stringtoint (char* a) {
-    int i;
-    int index = 0;
-    int add;
-    for (i = 0; i < strlen(a); i++){
-       add = a[i] -48;
-       index +=  (add)*( pow( 10, (strlen(a) -i -1)));
-    }
-    return index;
-}
-
-
-
-/*************************/
-/******** COMMAND ********/
-/*************************/
-
-/* freecmd */
-// DESCRIPTION      free the line allocated by getline()
-// INPUT            char *line    pointer to the line
-// RETURN           void
-void freecmd(char *line) {
-    free(line);
-    return;
-}
-
-/* getcmd */
-// DESCRIPTION  get a command from the user 
-// INPUT        char *prompt        prompt string to be displayed to the user
-//              char *args[]        pointer to the array in which the command is saved
-//              int *background     pointer to the value of background
-// RETURN       i                   the number of argument in the command  
-int getcmd(char *prompt, char *args[], int *background)
-{
-    int length, i = 0;
-    char *token, *loc;
-    char *line = NULL;
-    size_t linecap = 0;
-
-    printf("%s", prompt);
-    length = getline(&line, &linecap, stdin);
-
-    if (length <= 0) {
-        exit(-1);
-    }
-
-
-    // Check if background is specified..
-    if ((loc = index(line, '&')) != NULL) {
-        *background = 1;
-        *loc = ' ';
-    } else
-        *background = 0;
-
-    while ((token = strsep(&line, " \t\n")) != NULL) {
-        int j;
-        for (j = 0; j < strlen(token); j++)
-            if (token[j] <= 32)
-                token[j] = '\0';
-        if (strlen(token) > 0)
-            args[i++] = token;
-    }
-    freecmd(line);
-    return i;
-}
-
-/* initargs */
-// DESCRIPTION  initialize all the arguments of the command to null
-// INPUT        char *args[]    pointer to the arguments of the command
-// RETURN       void
-void initargs(char *args[]){
-    int i;
-    for (i = 0; i < 20; ++i)
-    {
-        args[i] = NULL;
-    }
-    return;
-}
-
-
-
-/**********************/
-/******** Main ********/
-/**********************/
 
 int main()
 {
@@ -493,8 +210,7 @@ int main()
                 //     }
                 // }
                 // else if (bg) {     
-                //     fclose(stdin);        
-                //     fclose(stdout);  
+                //     fclose(stdin);         
                 //     fclose(stderr); 
                 }
 
@@ -508,4 +224,214 @@ int main()
     }
 }
 
-//Wnohang
+
+/* JOB -------------------------------------------------------------------------*/
+
+void removejob(struct job ** head, int pid){
+    struct job * current = *head;
+    struct job * temp_job = NULL;
+
+    // if the job to be removed is the head
+    if (current->pid == pid){
+        temp_job = *head;
+        *head = (*head)->next;
+        free(temp_job);
+        return;
+    }
+
+    // if not we need to find where is the job
+    while(current->next != NULL && current->next->pid != pid){
+        current = current->next;
+    }
+    if(current->next != NULL){
+        temp_job = current->next;
+        current->next = temp_job->next;
+        free(temp_job);
+    }
+
+}
+
+void printjobs(struct job ** head) {
+    int i = 0;
+    int status;
+    struct job * current = *head;
+
+    // check if any job need to be printed
+    if (current == NULL) {
+        printf("No job in background.\n");
+    }
+    else{
+        printf("Jobs PID\t\tJobs\n----------------------------------------------------------\n");
+    }
+    while (current != NULL) {
+        printf("%i\t\t", current->pid);
+
+        int j = 0;
+        while(current->args[j] !=  NULL){
+            printf("%s\t", current->args[j]);
+            j++;
+        }
+        printf("\n");
+
+        // check if the process is terminated
+        // if it is it is removed from the jobs list
+        int pid = current->pid;
+        current = current->next;
+        waitpid(pid, &status, WNOHANG);
+        if(waitpid(pid, &status, WNOHANG) == -1){
+            removejob(head, pid);
+        }
+    }
+}
+
+void pushjob(struct job ** head, int pid, char *args[]) {
+    struct job * new_job;
+    new_job = malloc(sizeof(struct job));
+
+    new_job->pid = pid;
+    new_job->next = *head;
+    int j = 0 ;
+    for (j = 0; j < 20; j++) {
+        new_job->args[j] = args[j];
+    }
+    *head = new_job;
+}
+
+
+/* HISTORY -----------------------------------------------------------------------*/
+
+void addhistory(struct cmd *history[], char *args[], int nbr){
+
+    // delete the first element of history and add the new command at position 9
+    if (nbr > 10){
+        int j;
+        for (j = 0; j< 9; j++){
+            history[j] = history[j+1];
+        }
+        history[9] = (struct cmd*) malloc(sizeof(struct cmd));
+        for (j = 0; j < 20; j++) {
+            history[9]->args[j] = args[j];
+        }
+        history[9]->nbr = nbr;
+    }
+
+    // add the new command at the first empty spot in the history
+    else{
+        int j;
+        for (j = 0; j < 20; j++) {
+            history[nbr -1]->args[j] = args[j];
+        }
+        history[nbr -1]->nbr = nbr;
+    }
+    return;
+}
+
+int searchhistory(struct cmd *history[], int nbr) {
+    int i;
+    for (i = 0; i < 10; ++i)
+    {
+        if (history[i]->nbr == nbr){
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int isnumber (char *string) {
+    int num;
+    num = atoi( string );
+ 
+    if (num == 0 && string[0] != '0')
+       return 0;    //false
+    else
+       return 1;    //true
+}
+
+void printhistory(struct cmd *history[]) {
+    int i = 0;
+
+    // check if there is any history to be printed
+    if (history[0]->nbr == 0) {
+        printf("Nothing in history.\n");
+    }
+    else{
+        printf("History ID\t\tCommand\n----------------------------------------------------------\n");
+    }
+    while (i < 10 && history[i]->nbr != 0) {
+        printf("%i\t\t\t", history[i]->nbr);
+        int j = 0;
+        while(history[i]->args[j] !=  NULL){
+            printf("%s\t", history[i]->args[j]);
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+}
+
+int stringtoint (char* a) {
+    int i;
+    int index = 0;
+    int add;
+    for (i = 0; i < strlen(a); i++){
+
+        // from the ascci of the char get the number
+        add = a[i] -48;
+
+        // multiply by some power of 10 if required and add to the final number
+        index +=  (add)*( pow( 10, (strlen(a) -i -1)));
+    }
+    return index;
+}
+
+
+/* COMMAND -----------------------------------------------------------------------*/
+
+void freecmd(char *line) {
+    free(line);
+    return;
+}
+
+int getcmd(char *prompt, char *args[], int *background) {
+    int length, i = 0;
+    char *token, *loc;
+    char *line = NULL;
+    size_t linecap = 0;
+
+    printf("%s", prompt);
+    length = getline(&line, &linecap, stdin);
+
+    if (length <= 0) {
+        exit(-1);
+    }
+
+
+    // Check if background is specified..
+    if ((loc = index(line, '&')) != NULL) {
+        *background = 1;
+        *loc = ' ';
+    } else
+        *background = 0;
+
+    while ((token = strsep(&line, " \t\n")) != NULL) {
+        int j;
+        for (j = 0; j < strlen(token); j++)
+            if (token[j] <= 32)
+                token[j] = '\0';
+        if (strlen(token) > 0)
+            args[i++] = token;
+    }
+    freecmd(line);
+    return i;
+}
+
+void initargs(char *args[]){
+    int i;
+    for (i = 0; i < 20; ++i)
+    {
+        args[i] = NULL;
+    }
+    return;
+}
+
---
+++ oscode.h
@@ -0,0 +1,122 @@
+/**
+    *******************************************************************************
+    * File              : oscode.h
+    * Description       : Implementation of a simple shell
+    * Features          : History of the last 10 commands,
+                            background processes, list of jobs in background
+                            output redirection
+    * Author            : Felix Dube 260533620
+    * Version           : 1.0.0
+    * Date              : February 2016
+    *******************************************************************************
+    */
+
+
+/* JOB ---------------------------------------------------------------------------*/
+
+// store in a linked list all the jobs in background, along with their PID
+struct job
+{
+    char *args[20];
+    int pid;
+    struct job * next;
+};
+
+/**
+    * @brief    remove a job with a particuliar PID
+    * @param    head:   pointer to a pointer to the head of linked list of jobs
+    * @param    pid:    pid of the job to be removed from the linked list
+    * @retval   None
+    */
+void removejob(struct job ** head, int pid);
+
+/**
+    * @brief    print all the jobs in back ground and remove the job that are done
+    * @param    head:   linked list of job to be printed and updated
+    * @retval   None
+    */
+void printjobs(struct job ** head);
+
+/**
+    * @brief    add a job to the linked list
+    * @param    head:   pointer to a pointer to the head of linked list of jobs
+    * @param    pid:    pid of the job to be added
+    * @param    args:   command that the job is doing
+    * @retval   None
+    */
+void pushjob(struct job ** head, int pid, char *args[]);
+
+
+/* HISTORY -----------------------------------------------------------------------*/
+
+// store the command allong with its history ID
+struct cmd
+{
+    char *args[20];
+    int nbr;
+};
+
+/**
+    * @brief    add a specific cmd to the history    
+    * @param    history:    array in which the command need to be added
+    * @param    args:       command to be added
+    * @param    nbr:        history ID
+    * @retval   void
+    */
+void addhistory(struct cmd *history[], char *args[], int nbr);
+
+/**
+    * @brief    search for a specific history ID in the history
+    * @param    history:    history array to be search in
+    * @param    nbr:        history ID being search for
+    * @retval   if history ID found it returns the index of the cmd in the history array otherwise return -1        
+    */
+int searchhistory(struct cmd *history[], int nbr);
+
+/**
+    * @brief    verify if a string is a number
+    * @param    string:    pointer to the string to be verified
+    * @retval   1 if the string is a number, 0 otherwise
+    */
+int isnumber (char *string);
+
+/**
+    * @brief    print all commands in the history
+    * @param    history:    pointer to the history array
+    * @retval   None
+    */
+void printhistory(struct cmd *history[]);
+
+/**
+    * @brief    cast a string to int
+    * @param    a:     pointer to the string to be cast to int
+    * @retval   number that was in the string
+    */
+int stringtoint (char* a);
+
+
+/* COMMAND -----------------------------------------------------------------------*/
+
+/**
+    * @brief    free the line allocated by getline()
+    * @param    line:   pointer to the line
+    * @retval   None
+    */
+void freecmd(char *line);
+
+/**
+    * @brief    get a command from the user 
+    * @param    prompt:     prompt string to be displayed to the user
+    * @param    args:       pointer to the array in which the command is saved
+    * @param    background: pointer to the value of background
+    * @retval   the number of argument in the command  
+    */
+int getcmd(char *prompt, char *args[], int *background);
+
+/**
+    * @brief    initialize all the arguments of the command to null
+    * @param    args:   pointer to the arguments of the command
+    * @retval   None
+    */
+void initargs(char *args[]);
+
